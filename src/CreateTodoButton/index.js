221f9d4d1cad4e859b039f23166ffe2c1e7b0a41
Edward Mayk <samaelminina322@gmail.com>
2021-12-20 16:52:13 -0500

    v1

---
+++ src/CreateTodoButton/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './CreateTodoButton.css';
+
+function CreateTodoButton(props) {
+  const onCLickButton = () => {
+    props.setOpenModal(prevState => !prevState);
+  };
+
+
+    return (
+      <button 
+      className="CreateTodoButton"
+      onClick= {onCLickButton}
+      >
+        +
+        </button>
+      
+    );
+}
+
+export { CreateTodoButton };
